219cc4528dfac2ebff0916e45d83ee2bd8af758b
firyalmahakhan <firyalmahak@gmail.com>
2018-08-09 14:03:52 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -1,7 +1,12 @@
 def first_steps
   puts "Right foot back"
-  puts "Left foot back",
+  sleep(0.5)
+  puts "Left foot back"
+  sleep(0.5)
   puts "Right foot back and Stop"
+  sleep(0.5)
+  puts "Stop"
+  sleep(1)
 end
 end
 
